698f5e88658c1d259964b249b636b02ede6a6adf
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-07-08 18:14:48 +0530

    added userModel

---
+++ userService/src/models/userModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  otp?: string;
+  isVerified: boolean;
+}
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model<IUser>('User', userSchema);
